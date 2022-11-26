9eaac9f0e1823cdee0ac04b55f33f0f8bf51030d
VeraSlaveholder <bulgakova.vera2003@mail.ru>
2022-11-27 01:31:42 +0300

    task 2

---
+++ src/task2/Calendar.java
@@ -0,0 +1,14 @@
+package task2;
+
+public class Calendar {
+    public static void main(String[] args) {
+    }
+    public static int leapYearCount(int year) {
+        int i = 0;
+        for (int o = 1; o <= year; o++) {
+            if (((o % 4 == 0) && (o % 100 != 0)) || o % 400 == 0) {
+                i++;
+            }
+        }return i;
+    }
+}
