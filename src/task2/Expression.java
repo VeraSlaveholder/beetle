0433b8cac86999b4f8c5768941667cedfd44d9bf
VeraSlaveholder <bulgakova.vera2003@mail.ru>
2022-11-27 01:25:37 +0300

    task 1

---
+++ src/task2/Expression.java
@@ -0,0 +1,17 @@
+package task2;
+
+public class Expression {
+
+        public static void main(String[] args) {
+        }
+
+        public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
+            return (!a & !b & c & d)
+                    | (!a & b & !c & d)
+                    | (!a & b & c & !d)
+                    | (a & !b & !c & d)
+                    | (a & !b & c & !d)
+                    | (a & b & !c & !d);
+        }
+}
+
