7b9fd6e3f4b35536fced0d1ec3645d6ee0c94193
VeraSlaveholder <bulgakova.vera2003@mail.ru>
2022-11-27 02:08:38 +0300

    task

--- src/task1/Expression.java
+++ src/task1/Expression.java
@@ -3,7 +3,6 @@ package task1;
 public class Expression {
 
         public static void main(String[] args) {
-
         }
 
         public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
