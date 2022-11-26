ae84e920062c5dfbec8e43902ec8a722ec832b19
VeraSlaveholder <bulgakova.vera2003@mail.ru>
2022-11-27 01:31:11 +0300

    task 3

---
+++ src/task2/DoubleExpression.java
@@ -0,0 +1,13 @@
+package task2;
+
+public class DoubleExpression {
+    public static void main(String[] args) {
+    }
+    public static boolean doubleExpression(double a, double b, double c) {
+        if(Math.abs((a+b)-c)<0.000000001){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
