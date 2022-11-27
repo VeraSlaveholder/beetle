e72a1cf0728f95803e9e83ef6f8de63db17374dd
VeraSlaveholder <119085510+VeraSlaveholder@users.noreply.github.com>
2022-11-28 01:34:30 +0300

    Массивы и строки

---
+++ rubber/IsPalindrome.java
@@ -0,0 +1,12 @@
+package Task23;
+
+public class IsPalindrome {
+    public static boolean isPalindrome(String text) {
+        String newText=text.replaceAll("[^A-Za-z1-9]+", "");
+        byte a = 1;
+        byte b = 2;
+        byte c = (byte) (a + b);
+
+        return newText.equalsIgnoreCase(new StringBuilder(newText).reverse().toString());
+    }
+}
