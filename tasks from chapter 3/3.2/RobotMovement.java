99fca1e458175733f338bb44a252608aed241547
VeraSlaveholder <119085510+VeraSlaveholder@users.noreply.github.com>
2022-12-01 16:09:37 +0300

    Add files via upload

---
+++ tasks from chapter 3/3.2/RobotMovement.java
@@ -0,0 +1,45 @@
+package Task32;
+
+public class RobotMovement {
+    public static void main(String[] args) {
+        Robot robot = new Robot();
+        System.out.println(robot.getX());
+        System.out.println(robot.getY());
+        moveRobot(robot, -10, 20);
+        System.out.println(robot.getX());
+        System.out.println(robot.getY());
+    }
+
+    public static void moveRobot(Robot robot, int toX, int toY) {
+        if (robot.getX() < toX) {
+            do {
+                robot.turnRight();
+            } while (robot.getDirection() != Direction.RIGHT);
+            do {
+                robot.stepForward();
+            } while (robot.getX() != toX);
+        } else if (robot.getX() > toX) {
+            do {
+                robot.turnLeft();
+            } while (robot.getDirection() != Direction.LEFT);
+            do {
+                robot.stepForward();
+            } while (robot.getX() != toX);
+        }
+        if (robot.getY() < toY) {
+            do {
+                robot.turnRight();
+            } while (robot.getDirection() != Direction.UP);
+            do {
+                robot.stepForward();
+            } while (robot.getY()!=toY);
+        }else if(robot.getY() > toY) {
+            do {
+                robot.turnLeft();
+            } while (robot.getDirection() != Direction.DOWN);
+            do {
+                robot.stepForward();
+            } while (robot.getY()!=toY);
+        }
+    }
+}
