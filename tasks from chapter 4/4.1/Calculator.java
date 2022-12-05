555ad383a4493301f4fc7a490a563f6e8f2feeee
VeraSlaveholder <119085510+VeraSlaveholder@users.noreply.github.com>
2022-12-05 04:49:28 +0300

    Add files via upload

--- tasks from chapter 4/4.1/Calculator.java
+++ tasks from chapter 4/4.1/Calculator.java
@@ -4,12 +4,12 @@ public class Calculator {
     public static double sqrt(double x) {
         if (x < 0) {
             throw new IllegalArgumentException("Expected non-negative number, got " + x);
-        } else {
-            return Math.sqrt(x);
         }
+        return Math.sqrt(x);
+
     }
 
     public static void main(String[] args) {
-        System.out.println(sqrt(4.5));
+        System.out.println(sqrt(-4));
     }
 }
