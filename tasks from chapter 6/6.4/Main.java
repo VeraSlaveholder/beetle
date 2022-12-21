bf800a9903350859382b2bf3a93a2fa55761d73f
VeraSlaveholder <119085510+VeraSlaveholder@users.noreply.github.com>
2022-12-21 05:56:40 +0300

    Add files via upload

---
+++ tasks from chapter 6/6.4/Main.java
@@ -0,0 +1,11 @@
+package Task64;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Printing the file passed in:");
+        while(sc.hasNextLine()) System.out.println(sc.nextLine());
+    }
+}
---
+++ tasks from chapter 6/6.4/MinMax.java
@@ -0,0 +1,28 @@
+package Task64;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MinMax {
+    public static void main(String[] args) {
+        findMinMax(
+                Stream.of(9, 6, 8, 3, 19, 16, 18, 13),
+                Integer::compareTo,
+                (v1, v2) -> System.out.println("max = " + v2 + "\nmin = " + v1)
+        );
+    }
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+        List<T> items = stream.sorted(order).collect(Collectors.toList());
+        if (!items.isEmpty()) {
+            minMaxConsumer.accept(items.get(0), items.get(items.size() - 1));
+        } else minMaxConsumer.accept(null, null);
+    }
+}
+
